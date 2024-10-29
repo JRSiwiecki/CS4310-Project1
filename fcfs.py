01e065e419c676e205d44424d1d0fc087dbbfb8d
JRSiwiecki <JRSiwiecki@gmail.com>
2024-10-28 18:45:32 -0700

    Clean up FCFS implementation

--- fcfs.py
+++ fcfs.py
@@ -1,6 +1,35 @@
-def pick_next_job():
-    pass
+def run_scheduler(batch_list):
+    average_turnaround_times = []
+
+    for batch_number, job_list in enumerate(batch_list):
+        print("##### batch" + str(batch_number) + ".txt #####")
+        print(job_list)
+
+        completed_job_indexes = []
+        job_complete_time = 0
+
+        for job_number, job_time in enumerate(job_list):
+            print("-----------")
+            if job_number in completed_job_indexes:
+                continue
+
+            print("Job #" + str(job_number + 1) + " Scheduled")
+
+            job_time = work_on_job(job_time)
+
+            if job_time == 0:
+                print("Job #" + str(job_number + 1) + " Complete")
+                job_complete_time += job_list[job_number]
+                completed_job_indexes.append(job_number)
+
+        average_turnaround_time = round(job_complete_time / len(job_list), 2)
+        average_turnaround_times.append(average_turnaround_time)
+
+    return average_turnaround_times
 
 
-def work_on_job():
-    pass
+def work_on_job(job_time):
+    print("Doing " + str(job_time) + " Units of Work")
+    job_time = job_time - job_time
+    print("Time Left: " + str(job_time))
+    return job_time
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import job_input_reader
+import fcfs
 
 
 def main():
     batch_list = job_input_reader.job_file_to_array()
-    print(batch_list)
+    fcfs_atts = fcfs.run_scheduler(batch_list)
+    print("-----------")
+    print("Average Turnaround Time:", fcfs_atts)
 
 
 if __name__ == "__main__":
