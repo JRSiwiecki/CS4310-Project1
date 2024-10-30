7c1bd0c427d42b0f74616b634d30b51abf83a895
JRSiwiecki <JRSiwiecki@gmail.com>
2024-10-29 22:09:50 -0700

    Fix FCFS incorrect ATT bug

--- fcfs.py
+++ fcfs.py
@@ -1,31 +1,29 @@
 def run_scheduler(batch_list):
     print("----------- FCFS Scheduler -----------")
-
     average_turnaround_times = []
 
     for batch_number, job_list in enumerate(batch_list):
-        print("##### batch" + str(batch_number) + ".txt #####")
+        print("##### batch" + str(batch_number + 1) + ".txt #####")
         print(job_list)
-
+        job_list_copy = job_list.copy()
         completed_job_indexes = []
-        job_complete_time = 0
+        job_turnaround_times = []
+        current_time = 0
 
         for job_number, job_time in enumerate(job_list):
-            if job_number in completed_job_indexes:
-                continue
-
             print("-----------")
             print("Job #" + str(job_number + 1) + " Scheduled")
 
-            job_time = work_on_job(job_time)
+            current_time += job_time
+            job_list_copy[job_number] = 0
+            completed_job_indexes.append(job_number)
 
-            if job_time == 0:
-                print("Job #" + str(job_number + 1) + " Complete")
+            job_turnaround_times.append(current_time)
 
-                job_complete_time += job_list[job_number]
-                completed_job_indexes.append(job_number)
+            print("Job #" + str(job_number + 1) + " Complete")
+            print("Current Time:", current_time)
 
-        average_turnaround_time = round(job_complete_time / len(job_list), 2)
+        average_turnaround_time = round(sum(job_turnaround_times) / len(job_list), 2)
         average_turnaround_times.append(average_turnaround_time)
 
     print("-----------")
@@ -34,6 +32,4 @@ def run_scheduler(batch_list):
 
 def work_on_job(job_time):
     print("Doing " + str(job_time) + " Units of Work")
-    job_time = job_time - job_time
-    print("Time Left: " + str(job_time))
-    return job_time
+    return 0
