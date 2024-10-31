f7084f3bf65d3d9c77d8fe92b81d93f61a34a84b
JRSiwiecki <JRSiwiecki@gmail.com>
2024-10-31 14:22:43 -0700

    Add docstrings and comments to main.py

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ NUMBER_OF_BATCHES = 3
 
 
 def job_file_to_array():
+    """Convert batch job files (batch1.txt, batch2.txt, batch3.txt) to a list of
+    list of jobs.
+
+    Returns:
+        List[List[int]]: A list where each inner list contains int
+        values representing jobs.
+    """
     batch_list = []
 
     for i in range(1, NUMBER_OF_BATCHES + 1):
@@ -14,6 +21,8 @@ def job_file_to_array():
             job_list = []
 
             for line in file.readlines():
+                # Don't really care what the name/number of the job is, can just be
+                # tracked by its position in the list
                 if line[0] == "J":
                     continue
 
@@ -25,6 +34,9 @@ def job_file_to_array():
 
 
 def main():
+    """_summary_
+    Runs job_file_to_array then runs each scheduler.
+    """
     batch_list = job_file_to_array()
 
     fcfs.run_scheduler(batch_list)
