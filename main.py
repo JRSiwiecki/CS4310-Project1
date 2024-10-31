6279d15cb33475dbfe2cb9fd8282f22068dea1f9
JRSiwiecki <JRSiwiecki@gmail.com>
2024-10-31 13:51:55 -0700

    Add RR5 run to main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import job_input_reader
 import fcfs
 import sjf
 import rr2
+import rr5
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     fcfs.run_scheduler(batch_list)
     sjf.run_scheduler(batch_list)
     rr2.run_scheduler(batch_list)
+    rr5.run_scheduler(batch_list)
 
 
 if __name__ == "__main__":
